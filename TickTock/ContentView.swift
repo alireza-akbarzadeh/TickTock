ca6d8fb7c1600d3c33cdcd9e0248c18aed371130
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2025-03-07 13:53:25 +0330

    Initial Commit

---
+++ TickTock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TickTock
+//
+//  Created by ALIREZA AKBARZADEH  on 12/17/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TickTock/TickTockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockApp.swift
+//  TickTock
+//
+//  Created by ALIREZA AKBARZADEH  on 12/17/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct TickTockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
